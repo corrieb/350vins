1a4f929a1ecf74bb95d255bde2e85f0db7d29c16
Ben Corrie <bcorrie@vmware.com>
2020-06-11 11:07:56 -0700

    Improve update script

--- update.sh
+++ update.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-./pull-data.sh 500 599 > data/500-600-all.csv
-./pull-data.sh 600 699 > data/600-700-all.csv
-./pull-data.sh 700 799 > data/700-800-all.csv
-./pull-data.sh 800 899 > data/800-900-all.csv
-./pull-data.sh 900 999 > data/900-1000-all.csv
-./pull-data.sh 1000 1099 > data/1000-1100-all.csv
-./pull-data.sh 1000 1099 > data/1000-1100-all.csv
-./pull-data.sh 1100 1199 > data/1100-1200-all.csv
-./pull-data.sh 1200 1299 > data/1200-1300-all.csv
-./pull-data.sh 1300 1399 > data/1300-1400-all.csv
-./pull-data.sh 1400 1499 > data/1400-1500-all.csv
-./pull-data.sh 1500 1599 > data/1500-1600-all.csv
-./pull-data.sh 1600 1699 > data/1600-1700-all.csv
-./pull-data.sh 1700 1799 > data/1700-1800-all.csv
+vinrangefrom=5
+vinrangeto=18
+
+pullIfNotExists() {
+    if [ ! -f $3 ]; then
+        ./pull-data.sh $1 $2 > "$3.tmp"
+        mv "$3.tmp" $3
+    fi
+}
+
+if [ ! -d old_data ]; then
+    mv data old_data
+    mkdir data
+fi
+
+for i in $(seq $vinrangefrom $vinrangeto); 
+do
+    let start=$i*100
+    let end=$start+99
+    let rend=$end+1
+    pullIfNotExists $start $end data/$start-$rend-all.csv &
+done
+
+wait 
